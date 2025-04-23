f9abe8ec40493d17fdf98f09383f7a1048708ba5
vendidero <info@vendidero.de>
2025-04-23 13:18:58 +0200

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendidero/shiptastic-ups',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '91ba290e18b4a362f6e8c3c0f960eb28c9592369',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/jetpack-autoloader' => array(
+            'pretty_version' => 'v5.0.6',
+            'version' => '5.0.6.0',
+            'reference' => '5d533e1ff64ed0107b3b46928705fbfa8d3cdb1b',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vendidero/shiptastic-ups' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '91ba290e18b4a362f6e8c3c0f960eb28c9592369',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
